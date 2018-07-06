0e62e7919e8c82ee5e6b119453d3d55d70802139
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-07-06 11:27:46 -0400

    added heroku port

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 console.log('this works');
+
+
+app.get('/', (req, res)=>{
+  res.send('hi');
+})
+
+app.listen(PORT, ()=>{
+  console.log('hi');
+})
