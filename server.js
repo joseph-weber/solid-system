e5a754c024931c80a6cb6ad11865a798f7a6e05c
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-07-07 16:30:18 -0400

    added seed route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const User = require('./models/users.js');
 
 const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app'
@@ -16,8 +17,56 @@ app.get('/', (req, res)=>{
   res.render("index.ejs");
 })
 
+app.get('/seed/new', (req, res)=>{
+  User.create(
+    [
+    {
+      username: 'Stevie25',
+      password: 'billgleason',
+      name: 'Steve Mulcahy',
+      hours: [8, 9, 10, 11, 12, 1],
+      catPref: true,
+      dogPref: false,
+      fosterAble: true,
+      fostering: []
+    },
+    {
+      username: 'Jackattack713',
+      password: 'mogli',
+      name: 'Jackie Frumkin',
+      hours: [8, 9, 10, 11, 12, 1],
+      catPref: true,
+      dogPref: true,
+      fosterAble: true,
+      fostering: ['Salmon']
+    },
+    {
+      username: 'westsidestory4eva',
+      password: 'musicmansux',
+      name: 'Zach Frumkin',
+      hours: [8, 9, 10, 11, 12, 1],
+      catPref: false,
+      dogPref: true,
+      fosterAble: true,
+      fostering: []
+    },
+    {
+      username: 'canyouhangtonight',
+      password: 'ericaerickaarickaarica',
+      name: 'Steve Rubin',
+      hours: [],
+      catPref: false,
+      dogPref: false,
+      fosterAble: false,
+      fostering: ['Erica']
+    }
+  ],
+  (err, data)=>{
+             res.redirect('/');
+         }
+   )
+})
 
-app
 
 app.listen(PORT, ()=>{
   console.log('hi');
