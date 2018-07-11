9a6d88215fc7d1f6fa1a27e9b654859931a87a02
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-07-11 14:49:13 -0400

    completed index and show pages for events

--- controllers/events.js
+++ controllers/events.js
@@ -2,6 +2,25 @@ const express = require('express');
 const router = express.Router();
 const Event = require('../models/events.js');
 
+
+
+
+router.get('/', (req, res)=>{
+  Event.find({}, (err, foundEvents)=>{
+    res.render('events/index.ejs',
+  {
+    events: foundEvents,
+    currentUser: req.session.currentuser
+  }
+)
+});
+});
+
+
+
+
+
+
 router.get('/:id', (req, res)=>{
   Event.findOne({_id: req.params.id}, (err, foundEvent)=>{
     res.render('events/show.ejs',
--- models/events.js
+++ models/events.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const User = new Schema({
+  username: String,
+  password: String,
+  name: String,
+  hours: [Number],
+  catPref: Boolean,
+  dogPref: Boolean,
+  fosterAble: Boolean,
+  fostering: Array,
+  Admin: Boolean
+},
+{timestamps: true})
+
+
 
 const eventSchema = Schema({
   title: String,
   paragraph: String,
   img: String,
   date: String,
-  Location: String
+  Location: String,
+  attendees: [User],
 },
 {timestamps: true})
 
