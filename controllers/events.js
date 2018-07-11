9a4fb539ce215d36064eaf591e3e4f9cfe2ad849
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-07-11 18:00:19 -0400

    edit event and delete routes/buttons created

--- controllers/events.js
+++ controllers/events.js
@@ -16,7 +16,42 @@ router.get('/', (req, res)=>{
 });
 });
 
+router.get('/new', (req, res)=>{
+  res.render('events/new.ejs',
+  {
+    currentUser: req.session.currentuser
+  }
+);
+});
 
+router.post('/', (req, res)=>{
+  Event.create(req.body, (err, newEvent)=>{
+    res.redirect('/');
+  })
+});
+
+router.get('/:id/edit', (req, res)=>{
+  Event.findOne({_id: req.params.id}, (err, foundEvent)=>{
+    res.render('events/edit.ejs',
+  {
+    event: foundEvent,
+    currentUser: req.session.currentuser
+  }
+)
+});
+});
+
+router.put('/:id', (req, res)=>{
+  Event.findOneAndUpdate({_id: req.params.id}, req.body, {new:true}, (err, updatedEvent)=>{
+    res.redirect('/events')
+});
+});
+
+router.delete('/:id', (req, res)=>{
+  Event.findByIdAndRemove(req.params.id, (err, deletedEvent)=>{
+    res.redirect('/events')
+  });
+});
 
 
 
@@ -33,4 +68,12 @@ router.get('/:id', (req, res)=>{
 });
 
 
+router.post('/:id/attend', (req, res)=>{
+  Event.update({_id: req.params.id}, {$pull: {attendees: req.session.currentuser.name}}, (err, grub)=>{
+console.log('Grub');
+});
+  Event.update({_id: req.params.id}, {$push: {attendees: req.session.currentuser.name}},  (err, attendee)=>{
+  res.redirect('/events')
+});
+});
 module.exports = router;
